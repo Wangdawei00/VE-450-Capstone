078afdf561e9f2deb6086f5b4cf42e4c99e7db1d
Wangdawei <wdwdawei@umich.edu>
2022-06-28 16:11:12 -0400

    fix style

--- ASD/js/Calibration.jsx
+++ ASD/js/Calibration.jsx
@@ -9,55 +9,57 @@ const max_classify = 4
 
 
 class Calibration extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // seeso: new EasySeeSo(),
-      calibration_data: "",
+    constructor(props) {
+        super(props);
+        this.state = {
+            // seeso: new EasySeeSo(),
+            calibration_data: "",
+        }
+        this.Calibrate = this.Calibrate.bind(this);
     }
-    this.Calibrate = this.Calibrate.bind(this);
-  }
 
-  Calibrate() {
-    const {license} = this.props;
-    const userId = 'wdwdawei@umich.edu'; // ex) 5e9easf293
-    const redirectUrl = window.location.href;
-    const calibrationPoint = 5;
-    EasySeeSo.openCalibrationPage(license, userId, redirectUrl, calibrationPoint);
-  }
-
-  componentDidMount() {
-    const href = window.location.href
-    const decodedURI = decodeURI(href)
-    const queryString = decodedURI.split('?')[1];
-    if (!queryString) {
-      this.setState({calibration_data: ""})
-    } else {
-      this.setState({
-        calibration_data: queryString.slice("calibrationData=".length, queryString.length)
-      })
+    Calibrate() {
+        const { license } = this.props;
+        const userId = 'wdwdawei@umich.edu'; // ex) 5e9easf293
+        const redirectUrl = window.location.href;
+        const calibrationPoint = 5;
+        EasySeeSo.openCalibrationPage(license, userId, redirectUrl, calibrationPoint);
     }
-  }
 
-  render() {
-    const {license} = this.props;
-    const {calibration_data} = this.state;
-    if (calibration_data === "") {
-      return <div>
-        <Button onClick={this.Calibrate}>
-          开始校准
-        </Button>
-      </div>
-    } else {
-      return <Test calibration_data={calibration_data} license={license}
-                   max_classify={max_classify}
-                   max_stimuli_num={5} millisecond_per_stimuli={5000} millisecond_per_sample={100}/>
+    componentDidMount() {
+        const href = window.location.href
+        const decodedURI = decodeURI(href)
+        const queryString = decodedURI.split('?')[1];
+        if (!queryString) {
+            this.setState({ calibration_data: "" })
+        } else {
+            this.setState({
+                calibration_data: queryString.slice("calibrationData=".length, queryString.length)
+            })
+        }
     }
-  }
+
+    render() {
+        const { license } = this.props;
+        const { calibration_data } = this.state;
+        if (calibration_data === "") {
+            return <div class="container-row">
+                <div class="item-row">
+                    <Button onClick={this.Calibrate}>
+                        开始校准
+                    </Button>
+                </div>
+            </div>
+        } else {
+            return <Test calibration_data={calibration_data} license={license}
+                max_classify={max_classify}
+                max_stimuli_num={5} millisecond_per_stimuli={5000} millisecond_per_sample={100} />
+        }
+    }
 }
 
 Calibration.propTypes = {
-  license: PropTypes.string.required,
+    license: PropTypes.string.required,
 }
 
 export default Calibration;
--- ASD/js/form.jsx
+++ ASD/js/form.jsx
@@ -3,146 +3,166 @@ import Button from "react-bootstrap/Button";
 import Form from 'react-bootstrap/Form';
 
 class Forms extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email_state: "",
-      email_sent: false,
-      email_value: "",
-      otp: "",
-      password: "",
-      confirm_password: "",
-    };
-    this.onSubmitEmail = this.onSubmitEmail.bind(this);
-    this.onChangeEmail = this.onChangeEmail.bind(this);
-    this.onChangeOTP = this.onChangeOTP.bind(this);
-    this.onChangePassword = this.onChangePassword.bind(this);
-    this.onSubmitForm = this.onSubmitForm.bind(this);
-    this.onChangeConfirmPassword = this.onChangeConfirmPassword.bind(this);
-  }
-
-  onChangeEmail(event) {
-    event.preventDefault();
-    this.setState({
-      email_value: event.target.value,
-    });
-  }
-
-  onChangeOTP(event) {
-    event.preventDefault();
-    this.setState({
-      otp: event.target.value,
-    });
-  }
-
-  onChangePassword(event) {
-    event.preventDefault();
-    this.setState({
-      password: event.target.value,
-    });
-  }
-
-  onChangeConfirmPassword(event) {
-    event.preventDefault();
-    this.setState({
-      confirm_password: event.target.value,
-    });
-  }
-
-  onSubmitEmail() {
-    const {email_value} = this.state;
-    fetch("/api/v1/e/", {
-      credentials: "same-origin",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email_value,
-      }),
-    }).then((response) => {
-      if (!response.ok) {
-        this.setState({
-          email_state: "Press the button again to resend the email",
-        });
-      } else {
-        this.setState({
-          email_state: "Please check your email for one time password.",
-        });
-      }
-    });
-  }
-
-  onSubmitForm() {
-    const {email_value, otp, password, confirm_password} = this.state
-    if (confirm_password !== password) {
-      return
+    constructor(props) {
+        super(props);
+        this.state = {
+            email_state: "",
+            email_sent: false,
+            email_value: "",
+            otp: "",
+            password: "",
+            confirm_password: "",
+        };
+        this.onSubmitEmail = this.onSubmitEmail.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangeOTP = this.onChangeOTP.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmitForm = this.onSubmitForm.bind(this);
+        this.onChangeConfirmPassword = this.onChangeConfirmPassword.bind(this);
     }
-    fetch("/api/v1/r/", {
-      credentials: "same-origin",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email_value,
-        otp: otp,
-        password: password
-      })
-    }).then((response) => {
-      if (response.ok) {
-        window.location.replace("/")
-      } else {
-        alert("One time password invalid")
 
-        window.location.replace("/accounts/reset");
-      }
-    });
-  }
+    onChangeEmail(event) {
+        event.preventDefault();
+        this.setState({
+            email_value: event.target.value,
+        });
+    }
 
-  componentDidMount() {
-  }
+    onChangeOTP(event) {
+        event.preventDefault();
+        this.setState({
+            otp: event.target.value,
+        });
+    }
 
-  render() {
-    const {
-      email_state,
-      email_sent,
-      email_value,
-      otp,
-      password,
-      confirm_password,
-    } = this.state;
-    return (
-      <div>
-        <p>电子邮箱：</p>
-        <Form onSubmit={this.onSubmitEmail}>
-          <Form.Group>
-            <Form.Control type='text' value={email_value} onChange={this.onChangeEmail}/>
-          </Form.Group></Form>
-        <Button onClick={this.onSubmitEmail}>发送邮件</Button>
-        <Form onSubmit={this.onSubmitForm}>
-          <Form.Group>
-            <p>一次性密码：</p>
-            <Form.Control type="text" value={otp} onChange={this.onChangeOTP}/>
-            <p>新密码：</p>
-            <Form.Control type="password" value={password} onChange={this.onChangePassword}/>
-            <p>确认新密码：</p>
-            <Form.Control type="password" value={confirm_password} onChange={this.onChangeConfirmPassword}/>
-            {password === confirm_password && <p>密码匹配成功！</p>}
-            {password !== confirm_password && <p><b>密码不匹配</b></p>}
-          </Form.Group>
-        </Form>
-        {(password !== confirm_password || password === '') && (
-          <Button onClick={this.onSubmitForm} disabled>
-            创建/重设账号
-          </Button>
-        )}
-        {password === confirm_password && password !== '' && (
-          <Button onClick={this.onSubmitForm}>创建/重设账号</Button>
-        )}
-      </div>
-    );
-  }
+    onChangePassword(event) {
+        event.preventDefault();
+        this.setState({
+            password: event.target.value,
+        });
+    }
+
+    onChangeConfirmPassword(event) {
+        event.preventDefault();
+        this.setState({
+            confirm_password: event.target.value,
+        });
+    }
+
+    onSubmitEmail() {
+        const { email_value } = this.state;
+        fetch("/api/v1/e/", {
+            credentials: "same-origin",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: email_value,
+            }),
+        }).then((response) => {
+            if (!response.ok) {
+                this.setState({
+                    email_state: "Press the button again to resend the email",
+                });
+            } else {
+                this.setState({
+                    email_state: "Please check your email for one time password.",
+                });
+            }
+        });
+    }
+
+    onSubmitForm() {
+        const { email_value, otp, password, confirm_password } = this.state
+        if (confirm_password !== password) {
+            return
+        }
+        fetch("/api/v1/r/", {
+            credentials: "same-origin",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: email_value,
+                otp: otp,
+                password: password
+            })
+        }).then((response) => {
+            if (response.ok) {
+                window.location.replace("/")
+            } else {
+                alert("One time password invalid")
+
+                window.location.replace("/accounts/reset");
+            }
+        });
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        const {
+            email_state,
+            email_sent,
+            email_value,
+            otp,
+            password,
+            confirm_password,
+        } = this.state;
+        return (
+            <div class="container-row">
+                <div class="item-row container-col">
+                    <div class="item-col">
+                        <p>电子邮箱：</p>
+                        <Form onSubmit={this.onSubmitEmail}>
+                            <Form.Group>
+                                <Form.Control type='email' value={email_value} onChange={this.onChangeEmail} />
+                            </Form.Group>
+                        </Form>
+                    </div>
+
+                    <div class="item-col">
+                        <Button onClick={this.onSubmitEmail}>发送邮件</Button>
+                    </div>
+
+                    <Form onSubmit={this.onSubmitForm}>
+                        <Form.Group>
+                            <div class="item-col">
+                                <p>一次性密码：</p>
+                                <Form.Control type="text" value={otp} onChange={this.onChangeOTP} />
+                            </div>
+
+                            <div class="item-col">
+                                <p>新密码：</p>
+                                <Form.Control type="password" value={password} onChange={this.onChangePassword} />
+                            </div>
+
+                            <div class="item-col">
+                                <p>确认新密码：</p>
+                                <Form.Control type="password" value={confirm_password} onChange={this.onChangeConfirmPassword} />
+                            </div>
+
+                            {password === confirm_password && <p>密码匹配成功！</p>}
+                            {password !== confirm_password && <p><b>密码不匹配</b></p>}
+                        </Form.Group>
+                    </Form>
+
+                    {(password !== confirm_password || password === '') && (
+                        <Button onClick={this.onSubmitForm} disabled>
+                            创建/重设账号
+                        </Button>
+                    )}
+
+                    {password === confirm_password && password !== '' && (
+                        <Button onClick={this.onSubmitForm}>创建/重设账号</Button>
+                    )}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Forms;
