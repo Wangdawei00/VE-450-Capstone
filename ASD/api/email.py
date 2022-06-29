2d587aec2f8f9e22f4361f4792f9960672cb1573
yiyang <yiyangq@umich.edu>
2022-06-28 21:34:13 -0400

    use AWS SES to send email (WIP)

--- ASD/api/email.py
+++ ASD/api/email.py
@@ -3,6 +3,8 @@ import ASD
 import math
 import random
 from flask_mail import Message
+import boto3
+from botocore.exceptions import ClientError
 
 
 def generateOTP():
@@ -18,38 +20,102 @@ def generateOTP():
     return OTP
 
 
-@ASD.app.route('/api/v1/')
+@ASD.app.route("/api/v1/")
 def service():
-    return flask.jsonify({
-        'url': '/api/v1/',
-        'email': '/api/v1/e'
-    }), 200
+    return flask.jsonify({"url": "/api/v1/", "email": "/api/v1/e"}), 200
 
 
-@ASD.app.route('/api/v1/e/', methods=['POST'])
+@ASD.app.route("/api/v1/e/", methods=["POST"])
 def email():
-    if 'email' in flask.session:
-        return flask.jsonify({
-            "message": "Bad Request",
-            "status_code": 400
-        }), 400
-    user_email = flask.request.get_json()['email']
+    if "email" in flask.session:
+        return flask.jsonify({"message": "Bad Request", "status_code": 400}), 400
+    user_email = flask.request.get_json()["email"]
+
     connection = ASD.model.get_db()
-    msg = Message('Password Reset Email', sender='wdwdawei01@163.com', recipients=[user_email])
-    OTP = generateOTP()
-    msg.body = "One Time password: " + OTP
-    cur = connection.execute('SELECT COUNT(*) FROM users WHERE email = ?', (user_email,))
-    if cur.fetchone()['COUNT(*)'] == 0:
-        connection.execute("INSERT INTO users(email, OTP) VALUES (?,?)", (user_email, OTP))
+    cur = connection.execute(
+        "SELECT COUNT(*) FROM users WHERE email = ?", (user_email,)
+    )
+    if cur.fetchone()["COUNT(*)"] == 0:
+        connection.execute(
+            "INSERT INTO users(email, OTP) VALUES (?,?)", (user_email, OTP)
+        )
     else:
-        connection.execute("UPDATE users SET OTP = ? WHERE email = ?", (OTP, user_email))
+        connection.execute(
+            "UPDATE users SET OTP = ? WHERE email = ?", (OTP, user_email)
+        )
 
-    with open('log.txt', 'a') as log:
-        log.write(str(msg))
-        log.write("\n")
+    # msg = Message(
+    #     "Password Reset Email", sender="wdwdawei01@163.com", recipients=[user_email]
+    # )
+    # OTP = generateOTP()
+    # msg.body = "One Time password: " + OTP
 
-    ASD.mail.send(msg)
-    return flask.jsonify({
-        "message": "OK",
-        "status_code": 200
-    }), 200
+    # with open("log.txt", "a") as log:
+    #     log.write(str(msg))
+    #     log.write("\n")
+
+    # ASD.mail.send(msg)
+
+    # send the email with AWS SES
+    SENDER = "deluyijason@126.com"
+    RECIPIENT = flask.request.get_json()["email"]
+    AWS_REGION = "us-west-2"
+    SUBJECT = "Password Reset Email"
+
+    OTP = generateOTP()
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = "One Time password: " + OTP + "\r\n"
+
+    # The HTML body of the email.
+    BODY_HTML = """
+    <html>
+    <head></head>
+    <body>
+    <p>One Time password: %s </p>
+    </body>
+    </html>
+    """ % (
+        OTP
+    )
+
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client("ses", region_name=AWS_REGION)
+
+    # Try to send the email.
+    try:
+        # Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                "ToAddresses": [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                "Body": {
+                    "Html": {
+                        "Charset": CHARSET,
+                        "Data": BODY_HTML,
+                    },
+                    "Text": {
+                        "Charset": CHARSET,
+                        "Data": BODY_TEXT,
+                    },
+                },
+                "Subject": {
+                    "Charset": CHARSET,
+                    "Data": SUBJECT,
+                },
+            },
+            Source=SENDER,
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response["Error"]["Message"])
+    else:
+        print("Email sent! Message ID:"),
+        print(response["MessageId"])
+
+    return flask.jsonify({"message": "OK", "status_code": 200}), 200
