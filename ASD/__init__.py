764d8b425a807c949ba150ab7b4e79da58c1bb2a
yiyang <yiyangq@umich.edu>
2022-06-28 23:20:07 -0400

    revert fix

--- ASD/__init__.py
+++ ASD/__init__.py
@@ -1,7 +1,7 @@
 import flask
 from flask_mail import Mail
 
-app = flask.Flask(__name__, static_url_path="", static_folder="static")
+app = flask.Flask(__name__)
 app.config.from_object("ASD.config")
 mail = Mail(app)
 import ASD.api
