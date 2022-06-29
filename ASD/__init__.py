dda5f6f616c70cd502d50af76a50eb2f938efc83
yiyang <yiyangq@umich.edu>
2022-06-28 23:06:09 -0400

    fix AWS image serving (WIP)

--- ASD/__init__.py
+++ ASD/__init__.py
@@ -1,8 +1,8 @@
 import flask
 from flask_mail import Mail
 
-app = flask.Flask(__name__)
-app.config.from_object('ASD.config')
+app = flask.Flask(__name__, static_url_path="", static_folder="static")
+app.config.from_object("ASD.config")
 mail = Mail(app)
 import ASD.api
 import ASD.views
