a3df699e89182ec5f006bd2733588a37f00f7ade
yiyang <yiyangq@umich.edu>
2022-07-02 15:37:47 -0400

    fix

--- ASD/views/index.py
+++ ASD/views/index.py
@@ -9,8 +9,7 @@ def index():
     resp = flask.make_response(flask.render_template("index.html"))
     resp.headers["Cross-Origin-Opener-Policy"] = "same-origin"
     resp.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
-
-#     return resp
+    return resp
 # else:
 #     return flask.redirect(flask.url_for("login"))
 
